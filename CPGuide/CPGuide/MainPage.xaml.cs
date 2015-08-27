c071be81d159ecca86db96eadc92fd6c6f7f3822
Joel Braun <Joelbraun@outlook.com>
2015-08-26 17:05:20 -0700

    Working Hamburger Template
    Working hamburger menu template for use in the application. Still needs
    to be styled, have content added, etc.

---
+++ CPGuide/CPGuide/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CPGuide
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage(Frame frame)
+        {
+            this.InitializeComponent();
+            MainSplitView.Content = frame;
+            (MainSplitView.Content as Frame).Navigate(typeof(HomePage));
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+        }
+
+        private void Item1Click(object sender, RoutedEventArgs e)
+        {
+            (MainSplitView.Content as Frame).Navigate(typeof(HomePage));
+            HamburgerList.SelectedItem = Home;
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+        }
+
+        private void Item2Click(object sender, RoutedEventArgs e)
+        {
+            (MainSplitView.Content as Frame).Navigate(typeof(SecondPage));
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+            HamburgerList.SelectedItem = Microphone;
+        }
+    }
+}
